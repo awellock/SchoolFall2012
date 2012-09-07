3be68c1f5f647ed995c2a6780fac1c08d2e80785
alex <alex@alex-desktop.(none)>
2012-09-06 22:32:05 -0600

    added frist project for os

---
+++ os/QueueArray/include/QueueArray.h
@@ -0,0 +1,23 @@
+#ifndef QUEUEARRAY_H
+#define QUEUEARRAY_H
+
+template <class etype>
+class QueueArray
+{
+  public:
+    QueueArray(int);
+    ~QueueArray();
+    int Asize();
+    etype Dequeue();
+    int Enqueue(const etype &item,const int index);
+    int QAsize();
+    int Qsize(int index) ;
+    etype* Qstate(int index);
+
+  private:
+    int size;      //size of the array
+    queue<etype>* array;   //the array of queues
+    int totalItems; //total number of items stored in the queues
+};
+
+#endif // QUEUEARRAY_H
